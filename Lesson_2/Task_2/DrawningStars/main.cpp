eea930710381cf4e6d8601407173407f8c949cb1
aunadyy <aunadyy@gmail.com>
2024-10-16 15:22:29 +0300

    Add Lesson_2/Task_2

---
+++ Lesson_2/Task_2/DrawningStars/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int numbers[5];
+    int maxValue = 0;
+    cout << ' ' << endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << "Enter " << i + 1 << " value: ";
+        cin >> numbers[i];
+        if (numbers[i] > maxValue) {
+            maxValue = numbers[i];
+        }
+    }
+
+
+    for (int i = 0; i < maxValue; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            if (numbers[j] > i) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
